87e48893c4ed388ba090f675e96a23617df02f1a
JOSHUA DODGE WOODWARD <jdw6359@condor.(none)>
2014-03-10 20:53:57 -0400

    Methods all stubbed out and shit

--- hw4.c
+++ hw4.c
@@ -1,13 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
+
 
 #include "rootfinding.h"
 
+/* Function that represents the equation whose roots we are solving. Function
+   evaluates and returns result at the given x value */
 double equation(double value){
-	return 341.11;
+	return .76 * value * sin((30/52) * value) * tan((10/47) * value) + \
+	2.9 * cos(value + 2.5) * sin(.39 * (1.5 + value));
 }
 
+/* Function that represents the derivative of the equation expressed above.
+   Function evaluates and returns result at given x value */
 double derivative(double value){
 	return 911.350;
 }
@@ -20,7 +27,15 @@ int main(int argc, char *argv[]){
 	/* Declare string variable to represent the method */
 	char *method;
 
+	/* Declare double values for cmdline args */
+	double a, b, x0, x1, tolerance;
 
+	/* Declare int value for cmdline args */
+	int verbose;
+
+	/* Assign function addresses to variables */
+	func1arg f=&equation;
+	func1arg df=&derivative;
 
 	/* set method equal to the first applicable cmdline arg */
 	method=argv[1];
@@ -28,25 +43,42 @@ int main(int argc, char *argv[]){
 	/* Perform newtonian operation if the method is newton */
 	if(strcmp(method, "newton")==0){
 
-		func1arg f=&equation;
-		func1arg df=&derivative;
-		double guess, tolerance;
-		int verbose;
-		guess=atof(argv[2]);
-		tolerance=atof(argv[3]);
+		/* Assign newton parameters to cmdline args */
+		tolerance=atof(argv[2]);
+		x0=atof(argv[3]);
 		verbose=atoi(argv[4]);
 
-		newton(f, df, guess, MAX_ITERATIONS, tolerance, verbose);
+		/* Make function call to newton method */
+		newton(f, df, x0, MAX_ITERATIONS, tolerance, verbose);
 
 	}
 	/* End newton check */
 
 	if(strcmp(method, "bisection")==0){
 
+		/* Assign bisection parameters to cmdline args */
+		tolerance=atof(argv[2]);
+		a=atof(argv[3]);
+		b=atof(argv[4]);
+		verbose=atoi(argv[5]);
+
+		/* Make function call to newton method */
+
+		bisection(f,a,b,tolerance,verbose);
 	}
 	/* end bisection check */
 
 	if(strcmp(method, "secant")==0){
+
+		/* Assign secant parameters to cmdline args */
+		tolerance=atof(argv[2]);
+		x0=atof(argv[3]);
+		x1=atof(argv[4]);
+		verbose=atoi(argv[5]);
+
+
+
+		secant(f,x0,x1,tolerance,MAX_ITERATIONS,verbose);
 
 	}
 	/* end secant check */
